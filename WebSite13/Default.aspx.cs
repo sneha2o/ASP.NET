993451751e70f9f723036f0ed0e6117a51b6591a
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-10-16 00:15:22 +0530

    GU(Unit-2 = p1)

---
+++ WebSite13/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = DropDownList1.SelectedValue;
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int price = Convert.ToInt32(Label1.Text) * Convert.ToInt16(TextBox2.Text);
+        Label2.Text = Convert.ToString(price);
+    }
+}
